400ee6c0c03309e79fee9d80fddf06a657381769
ngnlananh <lanh111106@gmail.com>
2025-10-02 14:20:17 +0700

    Fix Bai03

--- BTH1_NguyenNgocLanAnh_24520109/Bai03/Program.cs
+++ BTH1_NguyenNgocLanAnh_24520109/Bai03/Program.cs
@@ -1,59 +1,36 @@
 ﻿using System;
-class Date
+class Program
 {
-    private int day;
-    private int month;
-    private int year;
+    public static void Main()
+    {
+        Console.Write("Nhap ngay: ");
+        int d = int.Parse(Console.ReadLine());
+        Console.Write("Nhap thang: ");
+        int m = int.Parse(Console.ReadLine());
+        Console.Write("Nhap nam: ");
+        int y = int.Parse(Console.ReadLine());
 
-    public Date(int d, int m, int y)
-    {
-        day = d; month = m; year = y;
+        bool Check = Check_HopLe(d, m, y);
+        if (Check)
+            Console.WriteLine("Ngay thang nam hop le");
+        else
+            Console.WriteLine("Ngay thang nam khong hop le");
     }
-    public Date()
-    {
-        day = DateTime.Now.Day;
-        month = DateTime.Now.Month;
-        year = DateTime.Now.Year;
-    }
-    bool Check_NamNhuan()
+    static bool Check_NamNhuan(int year)
     {
         if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))
             return true;
         return false;
     }
-    public bool CheckDate()
+    static bool Check_HopLe(int d, int m, int y)
     {
-        if (year < 1) return false;
-        if (month < 1 || month > 12) return false;
-        int[] Days_of_month = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-
-        if (Check_NamNhuan()) 
-            Days_of_month[1] = 29;
-
-        if (day < 1 || day > Days_of_month[month - 1])
+        if (y < 0 || m < 1 || m > 12 || d < 1)
+            return false;
+        int[] DaysofMonth = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+        if (m == 2 && Check_NamNhuan(y))
+            DaysofMonth[1] = 29;
+        if (DaysofMonth[m - 1] < d)
             return false;
         return true;
-    }
-    public void NhapDate()
-    {
-        Console.Write("Nhap ngay: ");
-        day = int.Parse(Console.ReadLine());
-        Console.Write("Nhap thang: ");
-        month = int.Parse(Console.ReadLine());
-        Console.Write("Nhap nam: ");
-        year = int.Parse(Console.ReadLine());
-    }
-}
-
-class Program
-{
-    public static void Main()
-    {
-        Date a = new Date();
-        a.NhapDate();
-        if (a.CheckDate())
-            Console.WriteLine("Ngay thang nam hop le");
-        else
-            Console.WriteLine("Ngay thang nam khong hop le");
     }
 }
