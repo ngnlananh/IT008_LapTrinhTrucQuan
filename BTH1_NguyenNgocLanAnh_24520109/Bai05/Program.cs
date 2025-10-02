75abadfa5d35fa97a0a7685d496305cf5e2021a8
ngnlananh <lanh111106@gmail.com>
2025-10-02 14:47:32 +0700

    update Bai05

--- BTH1_NguyenNgocLanAnh_24520109/Bai05/Program.cs
+++ BTH1_NguyenNgocLanAnh_24520109/Bai05/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+class Program
+{
+    public static void Main()
+    {
+        int m, y;
+        do
+        {
+            Console.Write("Nhap thang: ");
+            m = int.Parse(Console.ReadLine());
+            if (m < 1 || m > 12)
+                Console.WriteLine("Thang khong hop le. Vui long nhap lai!");
+        } while (m < 1 || m > 12);
+        do
+        {
+            Console.Write("Nhap nam: ");
+            y = int.Parse(Console.ReadLine());
+            if (y < 0)
+                Console.WriteLine("Nam khong hop le. Vui long nhap lai!");
+        } while (y < 0);
+
+        Console.WriteLine("So ngay trong thang {0} la: {1} ngay",
+            m, SoNgayTrongThang(m, y));
+    }
+    static bool Check_NamNhuan(int year)
+    {
+        if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))
+            return true;
+        return false;
+    }
+    static int SoNgayTrongThang(int m, int y)
+    {
+        int[] DaysofMonth = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+        if (m == 2 && Check_NamNhuan(y))
+            return 29;
+        return DaysofMonth[m - 1];
+    }
+    public static bool Check_HopLe(int day, int month, int year) 
+    {
+        int daysInMonth = SoNgayTrongThang(month, year);
+        return day >= 1 && day <= daysInMonth;
+    }
+    
+}
