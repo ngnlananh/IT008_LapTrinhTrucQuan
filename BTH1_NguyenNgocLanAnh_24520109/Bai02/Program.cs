cb5c72f88c498c9c67b250d6dfe034127d0cfff6
ngnlananh <lanh111106@gmail.com>
2025-10-02 11:29:51 +0700

    Bai02

---
+++ BTH1_NguyenNgocLanAnh_24520109/Bai02/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Program
+{
+    static int Tong_n(int n)
+    {
+        int sum = 0;
+        for (int i = 0; i < n; i++)
+            sum += i;
+        return sum;
+    }
+    public static void Main()
+    {
+        Console.Write("Nhap n: ");
+        int n = int.Parse(Console.ReadLine());
+        Console.WriteLine("Tong cac so nguyen to < n la: " + Tong_n(n));
+    }
+}
