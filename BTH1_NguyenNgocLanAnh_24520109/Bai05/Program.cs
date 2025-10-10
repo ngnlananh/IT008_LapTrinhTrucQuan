5f64fb5ebcddeb8410a4ab2c6401f5fbbac1ecda
ngnlananh <lanh111106@gmail.com>
2025-10-10 22:09:22 +0700

    Update Bai05

--- BTH1_NguyenNgocLanAnh_24520109/Bai05/Program.cs
+++ BTH1_NguyenNgocLanAnh_24520109/Bai05/Program.cs
@@ -3,24 +3,36 @@ class Program
 {
     public static void Main()
     {
-        int m, y;
+        int d, m, y;
         do
         {
+            Console.Write("Nhap ngay: ");
+            d = Convert.ToInt32(Console.ReadLine());
             Console.Write("Nhap thang: ");
-            m = int.Parse(Console.ReadLine());
-            if (m < 1 || m > 12)
-                Console.WriteLine("Thang khong hop le. Vui long nhap lai!");
-        } while (m < 1 || m > 12);
-        do
+            m = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Nhap nam: ");
+            y = Convert.ToInt32(Console.ReadLine());
+            if (!Check_HopLe(d, m, y))
+                Console.WriteLine("Ngay khong hop le, vui long nhap lai!");
+        } while (!Check_HopLe(d, m, y));
+        
+        int thu = DayofWeek(d, m, y);
+        if (thu == -1)
+            Console.WriteLine("Nam khong hop le");
+        else
         {
-            Console.Write("Nhap nam: ");
-            y = int.Parse(Console.ReadLine());
-            if (y < 0)
-                Console.WriteLine("Nam khong hop le. Vui long nhap lai!");
-        } while (y < 0);
-
-        Console.WriteLine("So ngay trong thang {0} la: {1} ngay",
-            m, SoNgayTrongThang(m, y));
+            Console.WriteLine("---Ngay {0}/{1}/{2} la:", d, m, y);
+            switch (thu)
+            {
+                case 0: Console.Write("Thu bay"); break;
+                case 1: Console.Write("Chu nhat"); break;
+                case 2: Console.Write("Thu hai"); break;
+                case 3: Console.Write("Thu ba"); break;
+                case 4: Console.Write("Thu tu"); break;
+                case 5: Console.Write("Thu nam"); break;
+                case 6: Console.Write("Thu sau"); break;
+            }
+        }
     }
     static bool Check_NamNhuan(int year)
     {
@@ -37,8 +49,23 @@ class Program
     }
     public static bool Check_HopLe(int day, int month, int year) 
     {
+        if (year < 1 || month < 1 || month > 12)
+            return false;
         int daysInMonth = SoNgayTrongThang(month, year);
-        return day >= 1 && day <= daysInMonth;
+        return (day >= 1 && day <= daysInMonth);
     }
-    
+    public static int DayofWeek (int day, int month, int year)
+    {
+        // ap dung cong thuc Zeller
+        int y = year;
+        int m = month;
+        if (m == 1 || m == 2) { m += 12; y--; }
+
+        int q = day;
+        int K = y % 100; //so nam trong the ky
+        int J = y / 100; // the ky
+
+        int h = (q + (13 * (m + 1)) / 5 + K + (K / 4) + (J / 4) + 5 * J) % 7;
+        return h;
+    }
 }
