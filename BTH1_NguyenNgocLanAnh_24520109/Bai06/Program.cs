a48741147b68ed5370d55c008662b556eda8bb13
ngnlananh <lanh111106@gmail.com>
2025-10-10 23:00:02 +0700

    Bai05 abcde

--- BTH1_NguyenNgocLanAnh_24520109/Bai06/Program.cs
+++ BTH1_NguyenNgocLanAnh_24520109/Bai06/Program.cs
@@ -1,2 +1,187 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Nhap n (n > 0):");
+        int n = int.Parse(Console.ReadLine());
+        Console.Write("Nhap m (m > 0):");
+        int m = int.Parse(Console.ReadLine());
+
+        int[][] arr = new int[n][];
+        TaoMaTran_NgauNhien(ref arr, n, m);
+
+        int choice;
+        do
+        {
+            Console.WriteLine("================= MENU ================");
+            Console.WriteLine("1. Xuat ma tran");
+            Console.WriteLine("2. Tim phan tu lon nhat / nho nhat");
+            Console.WriteLine("3. Tim dong co tong lon nhat");
+            Console.WriteLine("4. Tinh tong cac so khong phai la so nguyen to");
+            Console.WriteLine("5. Xoa dong thu k trong ma tran");
+            Console.WriteLine("6. Xoa cot chua phan tu lon nhat trong ma tran");
+            Console.WriteLine("0. Exit");
+            Console.Write("Chon chuc nang: ");
+
+            choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 0:
+                    Console.WriteLine("===========THANKYOU!===========");
+                    return;
+                case 1:
+                    XuatMaTran(arr);
+                    break;
+                case 2:
+                    Console.WriteLine("Phan tu lon nhat: " + Max_Element(arr));
+                    Console.WriteLine("Phan tu nho nhat: " + Min_Element(arr));
+                    break;
+                case 3:
+                    Console.WriteLine("Dong co tong lon nhat la dong thu: " + (DongCoTongLonNhat(arr) + 1));
+                    break;
+                case 4:
+                    Console.WriteLine("Tong cac so khong phai la so nguyen to: " + NumOfNonPrime(arr));
+                    break;
+                case 5:
+                    Console.Write("Nhap k (0 <= k < " + n + "): ");
+                    int k = int.Parse(Console.ReadLine());
+                    Erase_Row(ref arr, k);
+                    break;
+                case 6:
+                    Erase_Column(ref arr, Array.IndexOf(arr[0], Max_Element(arr)));
+                    break;
+                default:
+                    Console.WriteLine("Chuc nang khong hop le! Vui long chon lai");
+                    continue;
+            }
+        } while (choice != 0);
+    }
+
+    static void TaoMaTran_NgauNhien(ref int[][] arr, int n, int m)
+    {
+        Random rand = new Random();
+        for (int i = 0; i < n; i++)
+        {
+            arr[i] = new int[m];
+            for (int j = 0; j < m; j++)
+            {
+                arr[i][j] = rand.Next(0, 100);
+            }
+        }
+    }
+    static void XuatMaTran(int[][] arr)
+    {
+        Console.WriteLine("Ma tran:");
+        foreach (var row in arr)
+        {
+            foreach (var item in row)
+                Console.Write(item + " ");
+            Console.WriteLine();
+        }
+    }
+    static int Max_Element(int[][] arr)
+    {
+        int max = int.MinValue;
+        foreach (var row in arr)
+        {
+            foreach (var item in row)
+            {
+                if (item > max)
+                    max = item;
+            }
+        }
+        return max;
+    }
+    static int Min_Element(int[][] arr)
+    {
+        int min = int.MaxValue;
+        foreach (var row in arr)
+        {
+            foreach (var item in row)
+            {
+                if (item < min)
+                    min = item;
+            }
+        }
+        return min;
+    }
+    static int DongCoTongLonNhat(int[][] arr)
+    {
+        int maxSum = int.MinValue;
+        int rowIndex = -1;
+        for (int i = 0; i < arr.Length; i++)
+        {
+            int sum = 0;
+            for (int j = 0; j < arr[i].Length; j++)
+            {
+                sum += arr[i][j];
+            }
+            if (sum > maxSum)
+            {
+                maxSum = sum;
+                rowIndex = i;
+            }
+        }
+        return rowIndex;
+    }
+    static bool Check_SNT(int x)
+    {
+        if (x <= 1) return false;
+        if (x == 2) return true;
+        if (x % 2 == 0) return false;
+        else
+        {
+            for (int i = 3; i <= Math.Sqrt(x); i += 2)
+            {
+                if (x % i == 0)
+                    return false;
+            }
+        }
+        return true;
+    }
+    static int NumOfNonPrime(int[][] arr)
+    {
+        int count = 0;
+        foreach (var row in arr)
+        {
+            foreach (var item in row)
+            {
+                if (!Check_SNT(item))
+                    count++;
+            }
+        }
+        return count;
+    }
+    static void Erase_Row(ref int[][] arr, int k)
+    {
+        if (k < 0 || k >= arr.Length)
+        {
+            Console.WriteLine("Dong khong hop le.");
+            return;
+        }
+        for (int i = k; i < arr.Length - 1; i++)
+        {
+            arr[i] = arr[i + 1];
+        }
+        Array.Resize(ref arr, arr.Length - 1);
+    }
+    static void Erase_Column(ref int[][] arr, int colIndex)
+    {
+        if (arr.Length == 0 || colIndex < 0 || colIndex >= arr[0].Length)
+        {
+            Console.WriteLine("Cot khong hop le.");
+            return;
+        }
+        for (int i = 0; i < arr.Length; i++)
+        {
+            for (int j = colIndex; j < arr[i].Length - 1; j++)
+            {
+                arr[i][j] = arr[i][j + 1];
+            }
+            Array.Resize(ref arr[i], arr[i].Length - 1);
+        }
+    }
+}
