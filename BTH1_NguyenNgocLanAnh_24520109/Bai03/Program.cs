ca339b126da4d234fe14edc59e8967e1ea418139
ngnlananh <lanh111106@gmail.com>
2025-10-14 12:06:28 +0700

    Sửa bài 3, xây dựng thành class thời gian

--- BTH1_NguyenNgocLanAnh_24520109/Bai03/Program.cs
+++ BTH1_NguyenNgocLanAnh_24520109/Bai03/Program.cs
@@ -1,26 +1,40 @@
 ﻿using System;
-class Program
-{
-    public static void Main()
-    {
-        //Nhập ngày tháng năm 
-        Console.Write("Nhap ngay: ");
-        int d = int.Parse(Console.ReadLine());
-        Console.Write("Nhap thang: ");
-        int m = int.Parse(Console.ReadLine());
-        Console.Write("Nhap nam: ");
-        int y = int.Parse(Console.ReadLine());
+using System.Net.NetworkInformation;
 
-        //Kiểm tra tính hợp lệ 
-        bool Check = Check_HopLe(d, m, y);
-        if (Check)
-            Console.WriteLine("Ngay thang nam hop le");
-        else
-            Console.WriteLine("Ngay thang nam khong hop le");
+class cThoiGian
+{
+    private int iDay;
+    private int iMonth;
+    private int iYear;
+
+    //Hàm khởi tạo
+    public cThoiGian(int iDay = 0, int iMonth = 0, int iYear = 0)
+    {
+        this.iDay = iDay;
+        this.iMonth = iMonth;
+        this.iYear = iYear;
     }
 
+    //Nhập ngày tháng năm từ bàn phím
+    public void Nhap()
+    {
+        Console.Write("Nhập ngày:");
+        iDay = int.Parse(Console.ReadLine());
+        Console.Write("Nhập tháng:");
+        iMonth = int.Parse(Console.ReadLine());
+        Console.Write("Nhập năm:");
+        iYear = int.Parse(Console.ReadLine());
+    }
+
+    //Xuất ngày tháng năm
+    public void Xuat()
+    {
+        Console.Write("Ngày tháng năm vừa nhập là: ");
+        Console.WriteLine("{0}/{1}/{2}", iDay, iMonth, iYear);
+    }
+    
     //Kiểm tra năm nhuận
-    static bool Check_NamNhuan(int year)
+    public bool IsNamNhuan(int year)
     {
         if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))
             return true;
@@ -28,15 +42,34 @@ class Program
     }
 
     //Kiểm tra tính hợp lệ của ngày tháng năm
-    static bool Check_HopLe(int d, int m, int y)
+    public bool IsHopLe()
     {
-        if (y < 0 || m < 1 || m > 12 || d < 1)
+        if (iYear < 0 || iMonth < 1 || iMonth > 12 || iDay < 1)
             return false;
         int[] DaysofMonth = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-        if (m == 2 && Check_NamNhuan(y))
+        if (iMonth == 2 && IsNamNhuan(iYear))
             DaysofMonth[1] = 29;
-        if (DaysofMonth[m - 1] < d)
+        if (DaysofMonth[iMonth - 1] < iDay)
             return false;
         return true;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        //Nhập ngày tháng năm từ bàn phím
+        cThoiGian date = new cThoiGian();
+        Console.WriteLine("Nhập ngày tháng năm:");
+        date.Nhap();
+
+        //Xuất ngày tháng năm vừa nhập
+        date.Xuat();
+
+        //Xuất kết quả kiểm tra ngày tháng năm hợp lệ
+        if (date.IsHopLe() == true)
+            Console.WriteLine("Ngày tháng năm hợp lệ.");
+        else Console.WriteLine("Ngày tháng năm không hợp lệ.");
     }
 }
