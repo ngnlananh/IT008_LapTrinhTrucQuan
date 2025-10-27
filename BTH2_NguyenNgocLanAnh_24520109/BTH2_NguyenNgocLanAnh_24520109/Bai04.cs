a125f126bb0f7daf0555a16e21e21fa8d87b7648
ngnlananh <lanh111106@gmail.com>
2025-10-28 00:38:02 +0700

    Done Bai04

--- BTH2_NguyenNgocLanAnh_24520109/BTH2_NguyenNgocLanAnh_24520109/Bai04.cs
+++ BTH2_NguyenNgocLanAnh_24520109/BTH2_NguyenNgocLanAnh_24520109/Bai04.cs
@@ -2,9 +2,89 @@
 {
     class Bai04
     {
+        static cPhanSo NhapPhanSo()
+        {
+            //Nhập tử số
+            int iTu, iMau;
+            while (true)
+            {
+                Console.Write("Nhap tu so: ");
+                if (!int.TryParse(Console.ReadLine(), out iTu))
+                {
+                    Console.WriteLine("Vui long nhap lai tu so la so nguyen!");
+                    continue;
+                }
+                else
+                    break;
+            }
+
+            //Nhập mẫu số
+            while (true)
+            {
+                Console.Write("Nhap mau so: ");
+                if (!int.TryParse(Console.ReadLine(), out iMau) || iMau == 0)
+                {
+                    Console.WriteLine("Vui long nhap lai mau so la so nguyen (khac 0) !");
+                    continue;
+                }
+                else
+                    break;
+            }
+            return new cPhanSo(iTu, iMau);
+        }
         public static void Run()
         {
 
+            //nhập vào hai phân số, xuất ra các giá trị là tổng, hiệu, tích và thương của hai phân số này
+            
+            Console.WriteLine("Nhap phan so A:");
+            cPhanSo a = NhapPhanSo();
+            Console.WriteLine("Nhap phan so B:");
+            cPhanSo b = NhapPhanSo();
+
+            Console.WriteLine("Cac gia tri tong, hieu, tich thuong cua hai phan so tren: ");
+            Console.WriteLine("Tong: {0}", a + b);
+            Console.WriteLine("Hieu: {0}", a - b);
+            Console.WriteLine("Tich: {0}", a * b);
+            Console.WriteLine("Thuong: {0}", a / b);
+
+            // nhập vào một dãy các phân sô
+            int n;
+            while (true)
+            {
+                Console.Write("Nhap so luong phan so: ");
+                if (int.TryParse(Console.ReadLine(), out n) && n > 0)
+                    break;
+                else
+                {
+                    Console.Write("Vui long nhap lai so nguyen > 0.");
+                    continue;
+                }
+            }
+            
+            List<cPhanSo> list = new List<cPhanSo>(n);
+            for(int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Nhap phan so thu {0}: ", i + 1);
+                list.Add(NhapPhanSo());
+            }
+
+            //Tìm phân số lớn nhất
+            cPhanSo maxPS = list[0];
+            for(int i = 0; i < list.Count(); i++)
+            {
+                if (list[i].SoSanh(maxPS) > 0)
+                    maxPS = list[i];
+            }
+            Console.WriteLine("Phan so lon nhat trong day la: " + maxPS);
+
+            //Sắp xếp các phân số trong dãy tăng dần
+            list.Sort();
+            Console.WriteLine("Day phan so sau khi sap xep tang dan: ");
+            foreach(var item in list)
+            {
+                Console.WriteLine(item + "\t");
+            }
         }
     }
 }
--- BTH2_NguyenNgocLanAnh_24520109/BTH2_NguyenNgocLanAnh_24520109/cPhanSo.cs
+++ BTH2_NguyenNgocLanAnh_24520109/BTH2_NguyenNgocLanAnh_24520109/cPhanSo.cs
@@ -1,12 +1,98 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BTH2_NguyenNgocLanAnh_24520109
 {
-    internal class cPhanSo
+    internal class cPhanSo : IComparable<cPhanSo>
     {
+        private int iTu;
+        private int iMau;
+
+        //Hàm khởi tạo
+        public cPhanSo(int tu = 0, int mau = 1)
+        {
+            iTu = tu; iMau = mau;
+            RutGon();
+        }
+
+        private int UCLN(int a, int b)
+        {
+            while (b != 0)
+            {
+                int temp = a % b;
+                a = b;
+                b = temp;
+            }
+            return a;
+        }
+
+        private void RutGon()
+        {
+            int ucln = UCLN(Math.Abs(iTu), Math.Abs(iMau));
+            iTu /= ucln;
+            iMau /= ucln;
+
+            if (iMau < 0)
+            {
+                iTu = -iTu;
+                iMau = -iMau;
+            }
+        }
+
+        public static implicit operator cPhanSo(int x)
+        {
+            return new cPhanSo(x, 1);
+        }
+
+        //Operator công hai phân số
+        public static cPhanSo operator +(cPhanSo a, cPhanSo b)
+        {
+            return new cPhanSo(a.iTu * b.iMau + b.iTu * a.iMau, a.iMau * b.iMau);
+        }
+
+        //Operator trừ hai phân số
+        public static cPhanSo operator -(cPhanSo a, cPhanSo b)
+        {
+            return new cPhanSo(a.iTu * b.iMau - b.iTu * a.iMau, a.iMau * b.iMau);
+        }
+
+        //Operator nhân hai phân số 
+        public static cPhanSo operator *(cPhanSo a, cPhanSo b)
+        {
+            return new cPhanSo(a.iTu * b.iTu, a.iMau * b.iMau);
+        }
+
+        //Operator chia hai phân số
+        public static cPhanSo operator /(cPhanSo a, cPhanSo b)
+        {
+            if (b.iTu == 0)
+                throw new DivideByZeroException("Khong the chia cho 0");
+            return new cPhanSo(a.iTu * b.iMau, a.iMau * b.iTu);
+        }
+
+        //So sánh hai phân số
+        public int SoSanh (cPhanSo x)
+        {
+            double a = (double)this.iTu / this.iMau;
+            double b = (double)x.iTu / x.iMau;
+            return a.CompareTo(b);
+        }
+
+        public int CompareTo(cPhanSo? other)
+        {
+            if (other == null) return 1;
+            return SoSanh(other);
+        }
+
+        public override string ToString()
+        {
+            if (iMau == 1) 
+                return iTu.ToString();
+            return $"{iTu}/{iMau}";
+        }
     }
 }
