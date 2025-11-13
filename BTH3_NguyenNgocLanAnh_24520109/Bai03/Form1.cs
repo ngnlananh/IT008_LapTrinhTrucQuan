956c0c77a4c6139068604eaa9aa15b6078ea9367
ngnlananh <lanh111106@gmail.com>
2025-11-13 08:54:37 +0700

    update bai03

--- BTH3_NguyenNgocLanAnh_24520109/Bai03/Form1.cs
+++ BTH3_NguyenNgocLanAnh_24520109/Bai03/Form1.cs
@@ -17,6 +17,8 @@ namespace Bai03
             InitializeComponent();
             btnChangeColor.Text = "Change Color";
             btnChangeColor.Size = new Size(200, 50);
+            btnChangeColor.Location = new Point((this.ClientSize.Width - btnChangeColor.Width) / 2,
+                                              (this.ClientSize.Height - btnChangeColor.Height) / 2);
             btnChangeColor.Font = new Font("Courier New", 15, FontStyle.Bold);
         }
 
