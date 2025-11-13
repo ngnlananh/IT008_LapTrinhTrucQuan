a4f44120985cdfef034223827031be781d8c0ca0
ngnlananh <lanh111106@gmail.com>
2025-11-13 13:13:03 +0700

    bai07

--- BTH3_NguyenNgocLanAnh_24520109/Bai07/Form1.Designer.cs
+++ BTH3_NguyenNgocLanAnh_24520109/Bai07/Form1.Designer.cs
@@ -73,6 +73,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(53, 45);
             this.button1.TabIndex = 1;
+            this.button1.Tag = "1";
             this.button1.Text = "1";
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.btnGhe_Click);
@@ -86,6 +87,7 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(53, 45);
             this.button2.TabIndex = 2;
+            this.button2.Tag = "1";
             this.button2.Text = "2";
             this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.btnGhe_Click);
@@ -99,6 +101,7 @@
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(53, 45);
             this.button3.TabIndex = 3;
+            this.button3.Tag = "1";
             this.button3.Text = "3";
             this.button3.UseVisualStyleBackColor = false;
             this.button3.Click += new System.EventHandler(this.btnGhe_Click);
@@ -112,6 +115,7 @@
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(53, 45);
             this.button4.TabIndex = 4;
+            this.button4.Tag = "1";
             this.button4.Text = "4";
             this.button4.UseVisualStyleBackColor = false;
             this.button4.Click += new System.EventHandler(this.btnGhe_Click);
@@ -125,6 +129,7 @@
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(53, 45);
             this.button5.TabIndex = 5;
+            this.button5.Tag = "1";
             this.button5.Text = "5";
             this.button5.UseVisualStyleBackColor = false;
             this.button5.Click += new System.EventHandler(this.btnGhe_Click);
@@ -138,6 +143,7 @@
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(53, 45);
             this.button6.TabIndex = 6;
+            this.button6.Tag = "2";
             this.button6.Text = "6";
             this.button6.UseVisualStyleBackColor = false;
             this.button6.Click += new System.EventHandler(this.btnGhe_Click);
@@ -151,6 +157,7 @@
             this.button7.Name = "button7";
             this.button7.Size = new System.Drawing.Size(53, 45);
             this.button7.TabIndex = 7;
+            this.button7.Tag = "2";
             this.button7.Text = "7";
             this.button7.UseVisualStyleBackColor = false;
             this.button7.Click += new System.EventHandler(this.btnGhe_Click);
@@ -164,6 +171,7 @@
             this.button8.Name = "button8";
             this.button8.Size = new System.Drawing.Size(53, 45);
             this.button8.TabIndex = 8;
+            this.button8.Tag = "2";
             this.button8.Text = "8";
             this.button8.UseVisualStyleBackColor = false;
             this.button8.Click += new System.EventHandler(this.btnGhe_Click);
@@ -177,6 +185,7 @@
             this.button9.Name = "button9";
             this.button9.Size = new System.Drawing.Size(53, 45);
             this.button9.TabIndex = 9;
+            this.button9.Tag = "2";
             this.button9.Text = "9";
             this.button9.UseVisualStyleBackColor = false;
             this.button9.Click += new System.EventHandler(this.btnGhe_Click);
@@ -190,6 +199,7 @@
             this.button10.Name = "button10";
             this.button10.Size = new System.Drawing.Size(53, 45);
             this.button10.TabIndex = 10;
+            this.button10.Tag = "2";
             this.button10.Text = "10";
             this.button10.UseVisualStyleBackColor = false;
             this.button10.Click += new System.EventHandler(this.btnGhe_Click);
@@ -203,6 +213,7 @@
             this.button11.Name = "button11";
             this.button11.Size = new System.Drawing.Size(53, 45);
             this.button11.TabIndex = 11;
+            this.button11.Tag = "3";
             this.button11.Text = "11";
             this.button11.UseVisualStyleBackColor = false;
             this.button11.Click += new System.EventHandler(this.btnGhe_Click);
@@ -216,6 +227,7 @@
             this.button12.Name = "button12";
             this.button12.Size = new System.Drawing.Size(53, 45);
             this.button12.TabIndex = 12;
+            this.button12.Tag = "3";
             this.button12.Text = "12";
             this.button12.UseVisualStyleBackColor = false;
             this.button12.Click += new System.EventHandler(this.btnGhe_Click);
@@ -229,6 +241,7 @@
             this.button13.Name = "button13";
             this.button13.Size = new System.Drawing.Size(53, 45);
             this.button13.TabIndex = 13;
+            this.button13.Tag = "3";
             this.button13.Text = "13";
             this.button13.UseVisualStyleBackColor = false;
             this.button13.Click += new System.EventHandler(this.btnGhe_Click);
@@ -242,6 +255,7 @@
             this.button14.Name = "button14";
             this.button14.Size = new System.Drawing.Size(53, 45);
             this.button14.TabIndex = 14;
+            this.button14.Tag = "3";
             this.button14.Text = "14";
             this.button14.UseVisualStyleBackColor = false;
             this.button14.Click += new System.EventHandler(this.btnGhe_Click);
@@ -255,6 +269,7 @@
             this.button15.Name = "button15";
             this.button15.Size = new System.Drawing.Size(53, 45);
             this.button15.TabIndex = 15;
+            this.button15.Tag = "3";
             this.button15.Text = "15";
             this.button15.UseVisualStyleBackColor = false;
             this.button15.Click += new System.EventHandler(this.btnGhe_Click);
--- BTH3_NguyenNgocLanAnh_24520109/Bai07/Form1.cs
+++ BTH3_NguyenNgocLanAnh_24520109/Bai07/Form1.cs
@@ -12,29 +12,42 @@ namespace Bai07
 {
     public partial class fBai07 : Form
     {
-        const int cost = 100000;
         List<Button> selectedSeats = new List<Button>(); //List các ghế đang chọn 
-        int sum = 0;
         public fBai07()
         {
             InitializeComponent();
         }
+        private int SumCost()
+        {
+            int sum = 0;
+            foreach(Button btn in selectedSeats)
+            {
+                string row = (string)btn.Tag; //Lấy hàng của ghế
+                switch (row)
+                {
+                    case "1": sum += 5000; break;
+                    case "2": sum += 6500; break;
+                    case "3": sum += 8000; break;
+                }
+            }
+            return sum;
+        }
         private void btnGhe_Click (object sender, EventArgs e)
         {
             Button btn = (Button)sender;
-            if (btn.BackColor == Color.Blue) //Ghế đã bán
+            if (btn.BackColor == Color.Yellow) //Ghế đã bán
                 MessageBox.Show("Ghế đã được bán!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
             else if (btn.BackColor == Color.White) //Ghế chưa chọn
             {
-                btn.BackColor = Color.Yellow; //Chuyển sang màu ghế đang chọn
-                selectedSeats.Add(btn); //Thêm ghế vào danh sách ghế đang chọn
+                btn.BackColor = Color.Blue; //Chuyển sang màu ghế đang chọn
+                selectedSeats.Add(btn);
             }
-            else if (btn.BackColor == Color.Yellow) //Ghế đang chọn -> hủy chọn ghế
+            else if (btn.BackColor == Color.Blue) //Ghế đang chọn -> hủy chọn ghế
             {
                 btn.BackColor = Color.White; //Chuyển sang màu ghế chưa chọn
                 selectedSeats.Remove(btn); //Xóa ghế khỏi danh sách ghế đang chọn
             }
-            textBox1.Text = ((selectedSeats.Count * cost)).ToString();
+            textBox1.Text = SumCost().ToString(); //Cập nhật tổng tiền
         }
         private void btnChon_Click(object sender, EventArgs e)
         {
@@ -43,16 +56,17 @@ namespace Bai07
                 MessageBox.Show("Chưa có ghế nào được chọn!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 return;
             }
-            int totalCost = selectedSeats.Count * cost; //Tính tổng tiền
+            int totalCost = SumCost(); //Tính tổng tiền
             string seats = string.Join(", ", selectedSeats.Select(b => b.Text)); //Lấy danh sách ghế đã chọn
             DialogResult result = MessageBox.Show($"Bạn đã chọn ghế: {seats}\nTổng tiền: {totalCost} VND\nXác nhận đặt chỗ?", "Xác nhận", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
             if (result == DialogResult.Yes)
             {
                 foreach (Button btn in selectedSeats)
                 {
-                    btn.BackColor = Color.Blue; //Chuyển sang màu ghế đã bán
+                    btn.BackColor = Color.Yellow; //Chuyển sang màu ghế đã bán
                 }
                 selectedSeats.Clear(); //Xóa danh sách ghế đang chọn
+                textBox1.Text = "0";
                 MessageBox.Show("Đặt chỗ thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
         }
