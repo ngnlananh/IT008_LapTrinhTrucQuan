9c838870e0fc2553b9f0dd078e53d6f89168cbd6
ngnlananh <lanh111106@gmail.com>
2025-12-09 13:21:46 +0700

    Lab05-Bai06

---
+++ BTH5_NguyenNgocLanAnh_24520109/Bai06/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Bai06
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            listBox1.ItemHeight = 30;
+            listBox1.DrawMode = DrawMode.OwnerDrawFixed; // Cho phép vẽ tùy chỉnh
+            LoadFontFamilies();
+        }
+        private void LoadFontFamilies()
+        {
+            listBox1.Items.Clear();
+            foreach(FontFamily f in FontFamily.Families)
+            {
+                listBox1.Items.Add(f.Name);
+            }
+        }
+        private void listBox1_DrawItem(object sender, DrawItemEventArgs e)
+        {
+            if (e.Index >= 0 && e.Index <= listBox1.Items.Count)
+            {
+                // Lấy tên font
+                string fontName = listBox1.Items[e.Index].ToString();
+
+                // Tạo brush và FontStyle mặc định
+                Brush brush = SystemBrushes.ControlText;
+                FontStyle style = FontStyle.Regular;
+
+                // Thiết lập khi chọn item
+                e.DrawBackground();
+                if ((e.State & DrawItemState.Selected) == DrawItemState.Selected)
+                {
+                    brush = SystemBrushes.HighlightText;
+                }
+
+                // Apply font cho item
+                try
+                {
+                    Font itemFont = new Font(fontName, 14, style);
+                    e.Graphics.DrawString(fontName, itemFont, brush, e.Bounds.Location);
+                }
+                catch(Exception)
+                {
+                    // Nếu lỗi -> hiển thị lỗi font với font mặc định của Item
+                    e.Graphics.DrawString("(Lỗi font)", e.Font, brush, e.Bounds.Location);
+                }
+                
+                // Vẽ hình chữ nhật Focus khi được chọn
+                e.DrawFocusRectangle();
+            }
+        }
+    }
+}
--- BTH5_NguyenNgocLanAnh_24520109/Bai06/Program.cs
+++ BTH5_NguyenNgocLanAnh_24520109/Bai06/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Windows.Forms;
 
-namespace Bai04
+namespace Bai06
 {
     internal static class Program
     {
