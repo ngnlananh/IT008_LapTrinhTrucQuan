69f6551b260655a1a0db62b0358c4c24f33d6647
ngnlananh <lanh111106@gmail.com>
2025-12-09 12:20:37 +0700

    Lab05-bai1

---
+++ BTH5_NguyenNgocLanAnh_24520109/Bai04/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace Bai04
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ckbBold = new System.Windows.Forms.CheckBox();
+            this.ckbItalic = new System.Windows.Forms.CheckBox();
+            this.ckbUnderline = new System.Windows.Forms.CheckBox();
+            this.cmbFont = new System.Windows.Forms.ComboBox();
+            this.cmbSize = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rbRight = new System.Windows.Forms.RadioButton();
+            this.rbCenter = new System.Windows.Forms.RadioButton();
+            this.rbLeft = new System.Windows.Forms.RadioButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnColor = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Cambria", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(41, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Font";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Cambria", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(246, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 22);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Size";
+            // 
+            // ckbBold
+            // 
+            this.ckbBold.AutoSize = true;
+            this.ckbBold.Font = new System.Drawing.Font("Cambria", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ckbBold.Location = new System.Drawing.Point(43, 83);
+            this.ckbBold.Name = "ckbBold";
+            this.ckbBold.Size = new System.Drawing.Size(41, 26);
+            this.ckbBold.TabIndex = 2;
+            this.ckbBold.Text = "B";
+            this.ckbBold.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.ckbBold.UseVisualStyleBackColor = true;
+            this.ckbBold.CheckedChanged += new System.EventHandler(this.Style_CheckedChanged);
+            // 
+            // ckbItalic
+            // 
+            this.ckbItalic.AutoSize = true;
+            this.ckbItalic.Font = new System.Drawing.Font("Cambria", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ckbItalic.Location = new System.Drawing.Point(90, 83);
+            this.ckbItalic.Name = "ckbItalic";
+            this.ckbItalic.Size = new System.Drawing.Size(35, 26);
+            this.ckbItalic.TabIndex = 5;
+            this.ckbItalic.Text = "I";
+            this.ckbItalic.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.ckbItalic.UseVisualStyleBackColor = true;
+            this.ckbItalic.CheckedChanged += new System.EventHandler(this.Style_CheckedChanged);
+            // 
+            // ckbUnderline
+            // 
+            this.ckbUnderline.AutoSize = true;
+            this.ckbUnderline.Font = new System.Drawing.Font("Cambria", 14.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ckbUnderline.Location = new System.Drawing.Point(131, 83);
+            this.ckbUnderline.Name = "ckbUnderline";
+            this.ckbUnderline.Size = new System.Drawing.Size(41, 26);
+            this.ckbUnderline.TabIndex = 6;
+            this.ckbUnderline.Text = "U";
+            this.ckbUnderline.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.ckbUnderline.UseVisualStyleBackColor = true;
+            this.ckbUnderline.CheckedChanged += new System.EventHandler(this.Style_CheckedChanged);
+            // 
+            // cmbFont
+            // 
+            this.cmbFont.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.cmbFont.FormattingEnabled = true;
+            this.cmbFont.Location = new System.Drawing.Point(90, 35);
+            this.cmbFont.Name = "cmbFont";
+            this.cmbFont.Size = new System.Drawing.Size(138, 21);
+            this.cmbFont.TabIndex = 7;
+            // 
+            // cmbSize
+            // 
+            this.cmbSize.FormattingEnabled = true;
+            this.cmbSize.Location = new System.Drawing.Point(320, 34);
+            this.cmbSize.Name = "cmbSize";
+            this.cmbSize.Size = new System.Drawing.Size(62, 21);
+            this.cmbSize.TabIndex = 8;
+            this.cmbSize.TextChanged += new System.EventHandler(this.cmbSize_TextChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbRight);
+            this.groupBox1.Controls.Add(this.rbCenter);
+            this.groupBox1.Controls.Add(this.rbLeft);
+            this.groupBox1.Location = new System.Drawing.Point(35, 130);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(111, 113);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Align Text";
+            // 
+            // rbRight
+            // 
+            this.rbRight.AutoSize = true;
+            this.rbRight.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbRight.Location = new System.Drawing.Point(10, 79);
+            this.rbRight.Name = "rbRight";
+            this.rbRight.Size = new System.Drawing.Size(63, 23);
+            this.rbRight.TabIndex = 2;
+            this.rbRight.TabStop = true;
+            this.rbRight.Text = "Right";
+            this.rbRight.UseVisualStyleBackColor = true;
+            this.rbRight.CheckedChanged += new System.EventHandler(this.Alignment_CheckedChanged);
+            // 
+            // rbCenter
+            // 
+            this.rbCenter.AutoSize = true;
+            this.rbCenter.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbCenter.Location = new System.Drawing.Point(10, 50);
+            this.rbCenter.Name = "rbCenter";
+            this.rbCenter.Size = new System.Drawing.Size(73, 23);
+            this.rbCenter.TabIndex = 1;
+            this.rbCenter.TabStop = true;
+            this.rbCenter.Text = "Center";
+            this.rbCenter.UseVisualStyleBackColor = true;
+            this.rbCenter.CheckedChanged += new System.EventHandler(this.Alignment_CheckedChanged);
+            // 
+            // rbLeft
+            // 
+            this.rbLeft.AutoSize = true;
+            this.rbLeft.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbLeft.Location = new System.Drawing.Point(10, 21);
+            this.rbLeft.Name = "rbLeft";
+            this.rbLeft.Size = new System.Drawing.Size(54, 23);
+            this.rbLeft.TabIndex = 0;
+            this.rbLeft.TabStop = true;
+            this.rbLeft.Text = "Left";
+            this.rbLeft.UseVisualStyleBackColor = true;
+            this.rbLeft.CheckedChanged += new System.EventHandler(this.Alignment_CheckedChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Cambria", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(246, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 22);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Color";
+            // 
+            // btnColor
+            // 
+            this.btnColor.BackColor = System.Drawing.Color.Black;
+            this.btnColor.Location = new System.Drawing.Point(320, 83);
+            this.btnColor.Name = "btnColor";
+            this.btnColor.Size = new System.Drawing.Size(62, 23);
+            this.btnColor.TabIndex = 12;
+            this.btnColor.UseVisualStyleBackColor = false;
+            this.btnColor.Click += new System.EventHandler(this.btnColor_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(205, 136);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(237, 107);
+            this.richTextBox1.TabIndex = 13;
+            this.richTextBox1.Text = "Hello";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(483, 273);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btnColor);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.cmbSize);
+            this.Controls.Add(this.cmbFont);
+            this.Controls.Add(this.ckbUnderline);
+            this.Controls.Add(this.ckbItalic);
+            this.Controls.Add(this.ckbBold);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox ckbBold;
+        private System.Windows.Forms.CheckBox ckbItalic;
+        private System.Windows.Forms.CheckBox ckbUnderline;
+        private System.Windows.Forms.ComboBox cmbFont;
+        private System.Windows.Forms.ComboBox cmbSize;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rbRight;
+        private System.Windows.Forms.RadioButton rbCenter;
+        private System.Windows.Forms.RadioButton rbLeft;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnColor;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
